f7ea43b6854ed70cdb45c19fab5a6e3817a3f568
Tony Xavier <tony@brightowldigital.com>
2024-03-02 18:05:34 +0530

    Module updated

--- create-custom-module/app/code/TryNowPro/HelloWorld/Block/Index.php
+++ create-custom-module/app/code/TryNowPro/HelloWorld/Block/Index.php
@@ -3,4 +3,12 @@ namespace TryNowPro\HelloWorld\Block;
 class Index extends \Magento\Framework\View\Element\Template
 {
 
+    /**
+     * Get Hello World page content
+     * @return string
+     */
+    public function getContent()
+    {
+        return __("This is a custom module in Magento 2.");
+    }
 }
